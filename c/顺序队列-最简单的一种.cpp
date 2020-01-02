49a2bf070ed929011f2e8134cd4e52679f4f973f
jtjiang <jtjiangisme@outlook.com>
2020-01-02 16:00:13 +0800

    修改顺序队列-最简单的一种.cpp

--- c/顺序队列-最简单的一种.cpp
+++ c/顺序队列-最简单的一种.cpp
@@ -2,44 +2,47 @@
 /*最简单的一种顺序队列*/
 
 #include<stdio.h>
-#include<malloc.h>
-#include<stdlib.h>
+
+#define MAXSIZE 12
 
 typedef struct qnode
 {
-	int* data;
+	//int MAXSIZE;
+	int data[MAXSIZE];
 	int front, rear;
-	int MAXSIZE;
 }queue;
 
-queue init_queue(queue*, int); //初始化
+queue init_queue(queue*); //初始化
 bool is_full(queue*); //判满
 bool is_empty(queue*);//判空
-void enQueue(queue*, int); //进队列及元素
-void deQueue(queue*, int); //出队列及个数
+queue enQueue(queue*, int); //进队列及元素
+queue deQueue(queue*, int); //出队列及个数
 
 int main(void)
 {
 	queue Q;
-	init_queue(&Q, 12);
+	init_queue(&Q);
 	enQueue(&Q, 1);
+	enQueue(&Q, 3);
+	enQueue(&Q, 6);
+	enQueue(&Q, 7);
+	deQueue(&Q, 2);
+
 	return 0;
 }
 
-queue init_queue(queue* q, int MAX)
+queue init_queue(queue* q)
 {
-	q = (queue*)malloc(sizeof(queue));
-	q->data = (int*)malloc(sizeof(int) * MAX);
+	//q = (queue*)malloc(sizeof(queue));
 	q->front = 0;
 	q->rear = 0;
-	q->MAXSIZE = MAX;
 
 	return *q;
 }
 //判满
 bool is_full(queue* q)
 {
-	if (q->rear +1== q->MAXSIZE)
+	if (q->rear + 1 == MAXSIZE)
 		return true;
 	else
 		return false;
@@ -53,7 +56,7 @@ bool is_empty(queue* q)
 		return false;
 }
 //进队列
-void enQueue(queue* q, int e)
+queue enQueue(queue* q, int e)
 {
 	if (is_full(q))
 		printf("队满，无法写进元素！\n");
@@ -61,30 +64,32 @@ void enQueue(queue* q, int e)
 	{
 		q->rear++;
 		(q->data)[q->rear] = e;
-		printf("入队成功，入队元素为%d ,在第%d个位置", e, q->rear + 1);
+		printf("入队成功，入队元素为%d ,在第%d个位置\n", e, q->rear + 1);
+		return *q;
 	}
 }
 //出队
-void deQueue(queue* q, int x)
+queue deQueue(queue* q, int x)
 {
 	if (is_empty(q))
 		printf("队空，出队失败！\n");
 	else
 	{
-		
+
 		printf("出队元素为：");
 		for (int i = q->front; i < q->front + x; ++i)
-			printf("% d", q->data[i]);
+			printf("%d,", (q->data)[i]);
+		//printf("出队成功！\n");
 		q->front = q->front + x;
-
+		return *q;
 	}
 }
 /*
-	本段代码写于2020/1/1，在未正式成为计算机行业的一员的时候写的，其中有错误：
-		
-		（1）引发了未经处理的异常:写入访问权限冲突。
-		q->data 是 0x1110112。
-
-		（2）警告	C6011	取消对 NULL 指针“q”的引用。		第32行
-
+入队成功，入队元素为1 ,在第2个位置
+入队成功，入队元素为3 ,在第3个位置
+入队成功，入队元素为6 ,在第4个位置
+入队成功，入队元素为7 ,在第5个位置
+出队元素为：-858993460,1,
 */
+
+/*不知道为啥出队中，第一个值为垃圾值？现在能力有限，2020/1/2 */
